088ab6d97854829b12054e7fb18cf81176fa935b
Varad Mandhare <varadmandhare924@gmail.com>
2026-03-07 21:49:27 +0530

    Update clear_attendance.php

--- clear_attendance.php
+++ clear_attendance.php
@@ -1,11 +1,1 @@
-<?php
-include 'db.php';
 
-$sql = "DELETE FROM attendance";
-
-if ($conn->query($sql) === TRUE) {
-    echo "All attendance records cleared.";
-} else {
-    echo "Error: " . $conn->error;
-}
-?>
