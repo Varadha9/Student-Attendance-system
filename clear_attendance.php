a3d738f2a0af2720766e6272d46dc8767bb33313
Varad Mandhare <varadmandhare924@gmail.com>
2026-03-07 21:49:37 +0530

    Add script to clear all attendance records

--- clear_attendance.php
+++ clear_attendance.php
@@ -1,1 +1,12 @@
+<?php
+include 'db.php';
+
+$sql = "DELETE FROM attendance";
+
+if ($conn->query($sql) === TRUE) {
+    echo "All attendance records cleared.";
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
 
